ea2bcc56b9ef3238521db93b5e3e7959f58308b7
Manan Prjapati <mananprajapati1510@gmail.com>
2025-06-15 22:13:28 +0530

    v5 backend

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        const mongourl = (process.env.PRODUCTION == "true") ? process.env.MONGO_URI : process.env.DEPLOYMENT_MONGO_URL;
+        await mongoose.connect(mongourl);
         console.log("mongoDB is connected to Database");
     } catch (error) {
         console.log("Error in establishing connection " + error);
